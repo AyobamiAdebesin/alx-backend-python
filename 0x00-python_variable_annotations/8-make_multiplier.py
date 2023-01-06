8ac24e2f921304750e86e67e90b1da5d9ad1c646
AyobamiAdebesin <froshofficial@gmail.com>
2023-01-06 18:17:41 +0100

    Fixed pycodestyle error

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -8,6 +8,7 @@ by multiplier
 
 from typing import Callable, List, Union
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """ Return a function that multiplies a float by multiplier """
     return lambda x: x * multiplier
