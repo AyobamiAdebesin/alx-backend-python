93aa2cda55af930ae9f762f98a4c728105d7a483
AyobamiAdebesin <froshofficial@gmail.com>
2023-01-06 06:24:56 +0100

    sums a list of mixed types

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Performs summation on a list with different types """
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
+    return sum(mxd_list)
