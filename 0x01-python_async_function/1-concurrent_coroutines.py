f7b1aaff1ca9dc1d682030b4a4c75aa5bee39e57
AyobamiAdebesin <froshofficial@gmail.com>
2023-01-20 14:10:00 +0100

    Refactor: made use of asyncio method to return values

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -13,7 +13,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     delay_values: List[float] = []
     return_values: List[float] = []
     for _ in range(n):
-        delay_values.append(await wait_random(max_delay))
+        delay_values.append(wait_random(max_delay))
     for delay in asyncio.as_completed(delay_values):
         first_arrived = await delay
         return_values.append(first_arrived)
