c0b92671578cb1eb48d574897479daa507be52ce
AyobamiAdebesin <froshofficial@gmail.com>
2023-01-20 13:30:28 +0100

    Refactor: added type for return_values variables

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    return_values = []
+    return_values: List[float] = []
     for i in range(n):
         return_values.append(await wait_random(max_delay))
     return return_values
