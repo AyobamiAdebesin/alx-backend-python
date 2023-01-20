ba1ee6ad494511dce2347dde56fdb39e6ff0b0b2
AyobamiAdebesin <froshofficial@gmail.com>
2023-01-20 16:03:42 +0100

    First attempt to solve task

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" A function that returns an async.Task object """
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> float:
+    """ Returns an async.Task object """
+    task_object = asyncio.create_task(wait_random)
+    return task_object
