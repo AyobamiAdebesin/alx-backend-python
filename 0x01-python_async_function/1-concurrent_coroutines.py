068818c319136bf8aed3db3cfb776fa62ae51724
AyobamiAdebesin <froshofficial@gmail.com>
2023-01-20 13:05:28 +0100

    Finished asyncio task

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,14 @@
+""" Implementing concurrent coroutines """
+import asyncio
+import random
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    return_values = []
+    for i in range(n):
+        return_values.append(await wait_random(max_delay))
+    return return_values
