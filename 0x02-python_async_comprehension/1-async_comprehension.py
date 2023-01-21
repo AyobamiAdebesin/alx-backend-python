53122f1b233f9b8c1cc26bdfc0146f629e983f58
AyobamiAdebesin <froshofficial@gmail.com>
2023-01-21 22:01:19 +0100

    Removed redundant code

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -10,5 +10,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """ Use async comprehension to generate numbers and return them"""
-    numbers_arr: List[float] = [i async for i in async_generator()]
-    return numbers_arr
+    return [i async for i in async_generator()]
